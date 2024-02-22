ad366556084cc3c11ecd61668c536a62331cf5ef
Laura-Nyaaga <nyaagaaluoch@gmail.com>
2024-02-22 06:58:03 +0300

    add files

---
+++ src/Main.kt
@@ -0,0 +1,24 @@
+
+fun main(){
+  printFullName("Hello", "Laura")
+   remainder(20, 6)
+  sum(4,5,7,23)
+}
+fun printFullName(firstName: String, lastName: String){
+    var fullName = firstName+" "+ lastName
+    println(fullName)
+    statement("I","am", "smart")
+}
+fun remainder(num1: Int, num2: Int){
+   var result = num1%num2
+    println(result)
+}
+fun sum(num1: Int, num2: Int, num3: Int, num4: Int){
+    var result =num1 +num2 +num3 + num4
+   println(result)
+}
+fun statement(firstWord: String, secondWord: String, lastWord: String){
+   var result = firstWord+" "+ secondWord+" "+ lastWord
+   println(result)
+}
+
